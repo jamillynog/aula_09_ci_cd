a86a8ee6158c620e6bd5cd8c240c4ea4cc643751
ana maria nogueira <jamillynogu@gmail.com>
2023-05-04 09:23:26 -0300

    Funções Calculadora

--- methods.py
+++ methods.py
@@ -5,3 +5,20 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def addition(n1,n2):
+    soma = n1 + n2
+    return soma
+
+def subtraction(n1,n2):
+    sub = n1 - n2
+    return sub
+
+def multiplication(n1,n2):
+    mult = n1 * n2
+    return mult
+
+def division(n1,n2):
+    div = n1 / n2
+    return div
+
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,33 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    n1 = 45
+    n2 = 50
+    
+    output = methods.addition(n1,n2)
+    assert output == 95
+    
+def test_subtracao():
+    n1 = 70
+    n2 = 50
+    
+    output = methods.subtraction(n1,n2)
+    assert output == 20
+    
+def test_multiplicacao():
+    n1 = 5
+    n2 = 4
+    
+    output = methods.multiplication(n1,n2)
+    assert output == 20
+    
+def test_divisao():
+    n1 = 50
+    n2 = 10
+    
+    output = methods.division(n1,n2)
+    assert output == 5
+    
+    
